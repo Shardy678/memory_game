b51d272595eed15f9d25f1fcd6e7ae227aa3e4bf
Shardy678 <tambov.j678@gmail.com>
2021-11-28 08:41:32 +0300

    added shuffle cards func and card icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import "./App.css";
+import { useState } from "react";
+
+const cardIcons = [
+  { src: "/img/anchor.svg" },
+  { src: "/img/bicycle.svg" },
+  { src: "/img/house.svg" },
+  { src: "/img/plane.svg" },
+  { src: "/img/scissors.svg" },
+  { src: "/img/star.svg" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardIcons, ...cardIcons]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+  };
   return (
     <div className="App">
       <h1>MEMORY</h1>
